a2a4e383694f28f12da96402f3ddb81bc543bc29
WindyZY <2461607010@qq.com>
2018-12-05 18:03:36 +0800

    fighting!

--- 数据结构实验4/数据结构实验4/Hashdict.h
+++ 数据结构实验4/数据结构实验4/Hashdict.h
@@ -8,6 +8,12 @@
 // Source code Copyright (C) 2007-2011 by Clifford A. Shaffer.
 // Dictionary implemented with a hash table
 
+#include"ADT.h"
+#include"KVpair.h"
+#include<iostream>
+#include<ctime>
+#include<stdlib.h>
+
 template <typename Key, typename E>
 class hashdict : public Dictionary<Key, E> {
 private:
@@ -28,8 +34,61 @@ private:
 		return sum % M;
 	}
 
-	void hashInsert(const Key&, const E&) 
-	E hashSearch(const Key&) const 
+	int H2(int x) {//Hash function used in probing for int keys
+		return (x + 1)%(M - 2);
+	}
+
+	int H2(char* x) const {//Hash function used in probing for character keys
+		int i, sum;
+		for (sum = 0, i = 0; x[i] != '\0'; i++)
+			sum += (int)x[i];
+		return (sum + 1) % (M - 2);
+	}
+
+	int p(Key k, int i) {//
+		cout << "1.LinearProbe  2.QuadraicProbe  3.PseudorandomProbe" << endl;
+		int probe;
+		cin >> probe;
+		switch (probe)
+		{
+		case 1:
+			return (h(k) + i) % M;
+			break;
+		case 2:
+			return (h(k) + i*H2(k)) % M;
+			break;
+		case 3:
+			return (h(k) + rand()) % M;
+			break;
+		default:
+			cout << "WRONG!" << endl;
+			return -1;
+		}
+	}
+
+	// Insert e into hash table HT
+	void hashInsert(const Key& k, const E& e) {
+		int home;                     // Home position for e
+		int pos = home = h(k);        // Init probe sequence
+		for (int i = 1; EMPTYKEY != (HT[pos]).key(); i++) {
+			pos = (home + p(k, i)) % M; // probe
+			Assert(k != (HT[pos]).key(), "Duplicates not allowed");
+		}
+		KVpair<Key, E> temp(k, e);
+		HT[pos] = temp;
+	}
+	
+	// Search for the record with Key K
+	E hashSearch(const Key& k) const {
+		int home;              // Home position for k
+		int pos = home = h(k); // Initial position is home slot
+		for (int i = 1; (k != (HT[pos]).key()) &&
+			(EMPTYKEY != (HT[pos]).key()); i++)
+			pos = (home + p(k, i)) % M; // Next on probe sequence
+		if (k == (HT[pos]).key())     // Found it
+			return (HT[pos]).value();
+		else return NULL;            // k not in hash table
+	}
 
 public:
 	hashdict(int sz, Key k) { // "k" defines an empty slot
@@ -77,31 +136,4 @@ public:
 	}
 };
 
-// Insert e into hash table HT
-template <typename Key, typename E>
-void hashdict<Key, E>::
-hashInsert(const Key& k, const E& e) {
-	int home;                     // Home position for e
-	int pos = home = h(k);        // Init probe sequence
-	for (int i = 1; EMPTYKEY != (HT[pos]).key(); i++) {
-		pos = (home + p(k, i)) % M; // probe
-		Assert(k != (HT[pos]).key(), "Duplicates not allowed");
-	}
-	KVpair<Key, E> temp(k, e);
-	HT[pos] = temp;
-}
-
-// Search for the record with Key K
-template <typename Key, typename E>
-E hashdict<Key, E>::
-hashSearch(const Key& k) const {
-	int home;              // Home position for k
-	int pos = home = h(k); // Initial position is home slot
-	for (int i = 1; (k != (HT[pos]).key()) &&
-		(EMPTYKEY != (HT[pos]).key()); i++)
-		pos = (home + p(k, i)) % M; // Next on probe sequence
-	if (k == (HT[pos]).key())     // Found it
-		return (HT[pos]).value();
-	else return NULL;            // k not in hash table
-}
 #endif // !HASHDICT_H
--- 数据结构实验4/数据结构实验4/main.cpp
+++ 数据结构实验4/数据结构实验4/main.cpp
@@ -3,10 +3,14 @@
 #include<iostream>
 
 using namespace std;
+typedef int Key;
+typedef int E;
 
 int main()
 {
-
+	int M;
+	cin >> M;
+	hashdict<Key,E> *h = new hashdict<Key,E>(M,-1);
 	system("pause");
 	return 0;
 }
