8534d65cfac98ad40935a3382f114b25a2f811e6
WindyZY <2461607010@qq.com>
2018-12-07 16:10:46 +0800

    OK?

--- 数据结构实验4/数据结构实验4/main.cpp
+++ 数据结构实验4/数据结构实验4/main.cpp
@@ -22,7 +22,6 @@ int main()
 	Key k;
 	E e;
 	cout << "Enter the records need inserting,-1 to quit!" << endl;
-	//double start = clock();
 	while (cin >> k)
 	{
 		if (k == -1)
@@ -31,9 +30,6 @@ int main()
 		h->insert(k, e);
 	}
 	cout << "Insertion finished! *******************************" << endl << endl;
-
-	//double end = clock();
-	//cout << (double)(end - start) / CLOCKS_PER_SEC << endl;
 
 	//Test print function
 	h->print();
